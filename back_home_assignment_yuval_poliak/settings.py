08da2420428dd997f2499005a6f2264fdbdf4fea
yuval poliak <yuval.poliak5@gmail.com>
2024-07-18 22:33:22 +0300

    added dotenv.

--- back_home_assignment_yuval_poliak/settings.py
+++ back_home_assignment_yuval_poliak/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +29,7 @@ MEDIA_URL = '/media/'
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$23goh)27(&^e=&j5@_1%7jyfk9ed@vfgkgyko*x8ff%j!bxrl'
+SECRET_KEY = os.getenv('SECRET_KEY', default='')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
